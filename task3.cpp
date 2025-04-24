1cd7d99aacb211489e691f00837aa7c584cca5bc
Bibekpandeya91 <159869139+Bibekpandeya91@users.noreply.github.com>
2025-04-24 13:12:15 +0545

    task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -40,8 +40,8 @@ public:
     }
 
     void sortArray() {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) { 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) { 
                 if (a[i] < a[j]) {
                     int temp = a[i];
                     a[i] = a[j];
